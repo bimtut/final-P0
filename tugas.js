92a8b291c39012ba220ef9e16cdfc3c1ccf710f5
Bima Ketut Ramuna Putra <37969013+bimtut@users.noreply.github.com>
2018-12-24 17:49:18 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,258 @@
+var body = document.getElementsByTagName('body')[0]
+
+var bulet1 = document.getElementsByClassName('bulet1')[0]
+bulet1.addEventListener('click',function() {
+    body.style.backgroundColor = 'lightblue'
+    bulet1.style.border = '1px solid lightblue'
+})
+bulet1.addEventListener('mouseenter',function() {
+    bulet1.style.backgroundColor = 'lightblue'
+    bulet1.style.border= "1px solid black"
+    bulet1.style.opacity= 0.5
+})
+bulet1.addEventListener('mouseleave',function(){
+    bulet1.style.backgroundColor = 'lightblue'
+    bulet1.style.border= "1px solid #dedede"
+    bulet1.style.opacity= 1
+})
+// bulet1.style.backgroundColor = 'lightgreen'
+var bulet2 = document.getElementsByClassName('bulet2')[0]
+bulet2.addEventListener('click',function() {
+    body.style.backgroundColor = 'lightgreen'
+    bulet2.style.border = '1px solid lightgreen'
+})
+bulet2.addEventListener('mouseenter',function() {
+    bulet2.style.backgroundColor = 'lightgreen'
+    bulet2.style.border= "1px solid black"
+    bulet2.style.opacity= 0.5
+})
+bulet2.addEventListener('mouseleave',function(){
+    bulet2.style.backgroundColor = 'lightgreen'
+    bulet2.style.border= "1px solid #dedede"
+    bulet2.style.opacity= 1
+})
+var bulet3 = document.getElementsByClassName('bulet3')[0]
+bulet3.addEventListener('click',function() {
+    body.style.backgroundColor = 'pink'
+    bulet3.style.border = '1px solid pink'
+})
+bulet3.addEventListener('mouseenter',function() {
+    bulet3.style.backgroundColor = 'pink'
+    bulet3.style.border= "1px solid black"
+    bulet3.style.opacity= 0.5
+})
+bulet3.addEventListener('mouseleave',function(){
+    bulet3.style.backgroundColor = 'pink'
+    bulet3.style.border= "1px solid #dedede"
+    bulet3.style.opacity= 1
+})
+var bulet4 = document.getElementsByClassName('bulet4')[0]
+bulet4.addEventListener('click',function() {
+    body.style.backgroundColor = 'orange'
+    bulet4.style.border = '1px solid orange'
+})
+bulet4.addEventListener('mouseenter',function() {
+    bulet4.style.backgroundColor = 'orange'
+    bulet4.style.border= "1px solid black"
+    bulet4.style.opacity= 0.5
+})
+bulet4.addEventListener('mouseleave',function(){
+    bulet4.style.backgroundColor = 'orange'
+    bulet4.style.border= "1px solid #dedede"
+    bulet4.style.opacity= 1
+})
+var bulet5 = document.getElementsByClassName('bulet5')[0]
+bulet5.addEventListener('click',function() {
+    body.style.backgroundColor = 'lightcoral'
+    bulet5.style.border = '1px solid lightcoral'
+})
+bulet5.addEventListener('mouseenter',function() {
+    bulet5.style.backgroundColor = 'lightcoral'
+    bulet5.style.border= "1px solid black"
+    bulet5.style.opacity= 0.5
+})
+bulet5.addEventListener('mouseleave',function(){
+    bulet5.style.backgroundColor = 'lightcoral'
+    bulet5.style.border= "1px solid #dedede"
+    bulet5.style.opacity= 1
+})
+var bulet6 = document.getElementsByClassName('bulet6')[0]
+bulet6.addEventListener('click',function() {
+    body.style.backgroundColor = 'lightcyan'
+    bulet6.style.border = '1px solid lightcyan'
+})
+bulet6.addEventListener('mouseenter',function() {
+    bulet6.style.backgroundColor = 'lightcyan'
+    bulet6.style.border= "1px solid black"
+    bulet6.style.opacity= 0.5
+})
+bulet6.addEventListener('mouseleave',function(){
+    bulet6.style.backgroundColor = 'lightcyan'
+    bulet6.style.border= "1px solid #dedede"
+    bulet6.style.opacity= 1
+})
+var bulet7 = document.querySelectorAll('.bulet7')[0]
+bulet7.addEventListener('click',function() {
+    body.style.backgroundColor = 'lawngreen'
+    bulet7.style.border = '1px solid lawngreen'
+})
+bulet7.addEventListener('mouseenter',function() {
+    bulet7.style.backgroundColor = 'lawngreen'
+    bulet7.style.border= "1px solid black"
+    bulet7.style.opacity= 0.5
+})
+bulet7.addEventListener('mouseleave',function(){
+    bulet7.style.backgroundColor = 'lawngreen'
+    bulet7.style.border= "1px solid #dedede"
+    bulet7.style.opacity= 1
+})
+var bulet8 = document.querySelectorAll('.bulet8')[0]
+bulet8.addEventListener('click',function() {
+    body.style.backgroundColor = 'rgb(168, 98, 177)'
+    bulet8.style.border = '1px solid lawngreen'
+})
+bulet8.addEventListener('mouseenter',function() {
+    bulet8.style.backgroundColor = 'rgb(168, 98, 177)'
+    bulet8.style.border= "1px solid black"
+    bulet8.style.opacity= 0.5
+})
+bulet8.addEventListener('mouseleave',function(){
+    bulet8.style.backgroundColor = 'rgb(168, 98, 177)'
+    bulet8.style.border= "1px solid #dedede"
+    bulet8.style.opacity= 1
+})
+var bulet9 = document.querySelectorAll('.bulet9')[0]
+bulet9.addEventListener('click',function() {
+    body.style.backgroundColor = 'rgb(162, 170, 43)'
+    bulet9.style.border = '1px solid rgb(162, 170, 43)'
+})
+bulet9.addEventListener('mouseenter',function() {
+    bulet9.style.backgroundColor = 'rgb(162, 170, 43)'
+    bulet9.style.border= "1px solid black"
+    bulet9.style.opacity= 0.5
+})
+bulet9.addEventListener('mouseleave',function(){
+    bulet9.style.backgroundColor = 'rgb(162, 170, 43)'
+    bulet9.style.border= "1px solid #dedede"
+    bulet9.style.opacity= 1
+})
+var bulet10 = document.querySelectorAll('.bulet10')[0]
+bulet10.addEventListener('click',function() {
+    body.style.backgroundColor = 'rgb(82, 10, 38)'
+    bulet10.style.border = '1px solid rgb(82, 10, 38)'
+})
+bulet10.addEventListener('mouseenter',function() {
+    bulet10.style.backgroundColor = 'rgb(82, 10, 38)'
+    bulet10.style.border= "1px solid black"
+    bulet10.style.opacity= 0.5
+})
+bulet10.addEventListener('mouseleave',function(){
+    bulet10.style.backgroundColor = 'rgb(82, 10, 38)'
+    bulet10.style.border= "1px solid #dedede"
+    bulet10.style.opacity= 1
+})
+var bulet11 = document.querySelectorAll('.bulet11')[0]
+bulet11.addEventListener('click',function() {
+    body.style.backgroundColor = 'rgba(255, 51, 0, 0.986)'
+    bulet11.style.border = '1px solid rgba(255, 51, 0, 0.986)'
+})
+bulet11.addEventListener('mouseenter',function() {
+    bulet11.style.backgroundColor = 'rgba(255, 51, 0, 0.986)'
+    bulet11.style.border= "1px solid black"
+    bulet11.style.opacity= 0.5
+})
+bulet11.addEventListener('mouseleave',function(){
+    bulet11.style.backgroundColor = 'rgba(255, 51, 0, 0.986)'
+    bulet11.style.border= "1px solid #dedede"
+    bulet11.style.opacity= 1
+})
+var bulet12 = document.querySelectorAll('.bulet12')[0]
+bulet12.addEventListener('click',function() {
+    body.style.backgroundColor = 'rgb(213, 197, 221)'
+    bulet12.style.border = '1px solid rgb(213, 197, 221)'
+})
+bulet12.addEventListener('mouseenter',function() {
+    bulet12.style.backgroundColor = 'rgb(213, 197, 221)'
+    bulet12.style.border= "1px solid black"
+    bulet12.style.opacity= 0.5
+})
+bulet12.addEventListener('mouseleave',function(){
+    bulet12.style.backgroundColor = 'rgb(213, 197, 221)'
+    bulet12.style.border= "1px solid #dedede"
+    bulet12.style.opacity= 1
+})
+var bulet13 = document.querySelectorAll('.bulet13')[0]
+bulet13.addEventListener('click',function() {
+    body.style.backgroundColor = 'rgb(36, 204, 204)'
+    bulet13.style.border = '1px solid rgb(36, 204, 204)'
+})
+bulet13.addEventListener('mouseenter',function() {
+    bulet13.style.backgroundColor = 'rgb(36, 204, 204)'
+    bulet13.style.border= "1px solid black"
+    bulet13.style.opacity= 0.5
+})
+bulet13.addEventListener('mouseleave',function(){
+    bulet13.style.backgroundColor = 'rgb(36, 204, 204)'
+    bulet13.style.border= "1px solid #dedede"
+    bulet13.style.opacity= 1
+})
+var bulet14 = document.querySelectorAll('.bulet14')[0]
+bulet14.addEventListener('click',function() {
+    body.style.backgroundColor = 'rgb(109, 63, 33)'
+    bulet14.style.border = '1px solid rgb(109, 63, 33)'
+})
+bulet14.addEventListener('mouseenter',function() {
+    bulet14.style.backgroundColor = 'rgb(109, 63, 33)'
+    bulet14.style.border= "1px solid black"
+    bulet14.style.opacity= 0.5
+})
+bulet14.addEventListener('mouseleave',function(){
+    bulet14.style.backgroundColor = 'rgb(109, 63, 33)'
+    bulet14.style.border= "1px solid #dedede"
+    bulet14.style.opacity= 1
+})
+var bulet15 = document.querySelectorAll('.bulet15')[0]
+bulet15.addEventListener('click',function() {
+    body.style.backgroundColor = 'rgb(195, 173, 230)'
+    bulet15.style.border = '1px solid rgb(195, 173, 230)'
+})
+bulet15.addEventListener('mouseenter',function() {
+    bulet15.style.backgroundColor = 'rgb(195, 173, 230)'
+    bulet15.style.border= "1px solid black"
+    bulet15.style.opacity= 0.5
+})
+bulet15.addEventListener('mouseleave',function(){
+    bulet15.style.backgroundColor = 'rgb(195, 173, 230)'
+    bulet15.style.border= "1px solid #dedede"
+    bulet15.style.opacity= 1
+})
+var bulet16 = document.querySelectorAll('.bulet16')[0]
+bulet16.addEventListener('click',function() {
+    body.style.backgroundColor = 'rgb(5, 88, 5)'
+    bulet16.style.border = '1px solid rgb(5, 88, 5)'
+})
+bulet16.addEventListener('mouseenter',function() {
+    bulet16.style.backgroundColor = 'rgb(5, 88, 5)'
+    bulet16.style.border= "1px solid black"
+    bulet16.style.opacity= 0.5
+})
+bulet16.addEventListener('mouseleave',function(){
+    bulet16.style.backgroundColor = 'rgb(5, 88, 5)'
+    bulet16.style.border= "1px solid #dedede"
+    bulet16.style.opacity= 1
+})
+var bulet17 = document.querySelectorAll('.bulet17')[0]
+bulet17.addEventListener('click',function() {
+    body.style.backgroundColor = 'rgb(111, 48, 148)'
+    bulet17.style.border = '1px solid rgb(111, 48, 148)'
+})
+bulet17.addEventListener('mouseenter',function() {
+    bulet17.style.backgroundColor = 'rgb(111, 48, 148)'
+    bulet17.style.border= "1px solid black"
+    bulet17.style.opacity= 0.5
+})
+bulet17.addEventListener('mouseleave',function(){
+    bulet17.style.backgroundColor = 'rgb(111, 48, 148)'
+    bulet17.style.border= "1px solid #dedede"
+    bulet17.style.opacity= 1
+})
